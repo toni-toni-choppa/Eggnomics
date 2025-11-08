78be93a83c9839ce4381cd9590b2e257c278983f
toni-toni-choppa <241455787+toni-toni-choppa@users.noreply.github.com>
2025-11-08 15:52:41 -0500

    Add MySQL Connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,22 @@
 import { Router } from 'express';
+import connection from './mysql.js';
 const router = Router();
 
 router.get('/', (req, res) => {
     res.json({ status: 200, message: 'OK', details: null });
+});
+
+router.get('/status', (req, res) => {
+    try {
+        connection.ping((error) => {
+            if (error) {
+                return res.status(500).json({ status: 500, message: 'Internal Server Error', details: error.message });
+            }
+            res.json({ status: 200, message: 'OK', details: null });
+        });
+    } catch (error) {
+        res.status(500).json({ status: 500, message: 'Internal Server Error', details: error.message });
+    }
 });
 
 export default router;
